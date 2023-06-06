9e17119216babd79167269cc417bc06d794b8223
ayoub <ayoubaitelaouad@gmail.com>
2023-06-07 00:10:12 +0100

    hot fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,5 +4,7 @@ arr = [ord('z'), ord('Z')]
 toggle = 0
 for i in range(26):
     print("{}".format(chr(arr[toggle] - i)), end="")
-    if toggle == 0: toggle = 1
-    else: toggle = 0
+    if toggle == 0:
+        toggle = 1
+    else:
+        toggle = 0
