e107e241e7ec002a1991c87924ba2e65b2868477
ayoub <ayoubaitelaouad@gmail.com>
2023-06-06 21:26:58 +0100

    hot fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if (i != 99):
-        print(i, end=", ")
+        print("{}, ".format(i), end="")
     else:
-        print(i)
+        print("{}".format(i))
