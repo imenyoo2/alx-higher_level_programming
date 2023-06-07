f912855c4c290262dcef2f1335828af1d185565b
ayoub <ayoubaitelaouad@gmail.com>
2023-06-07 12:21:50 +0100

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,4 +2,24 @@
 
 int check_cycle(listint_t *list)
 {
+	listint_t *previous = list;
+
+	if (list == NULL)
+	{
+		return (0);
+	}
+	while (list != NULL)
+	{
+		list = list->next;
+		if (previous == NULL)
+		{
+			return (0);
+		}
+		if (previous < list)
+		{
+			return (1);
+		}
+		previous = list;
+	}
+	return (0);
 }
