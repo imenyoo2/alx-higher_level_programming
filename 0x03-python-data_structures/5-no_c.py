2b59f9af4c76577c9649474a3291e9bccbe821d4
ayoub <ayoubaitelaouad@gmail.com>
2023-06-10 23:36:33 +0100

    hot fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     clearedString = ""
     for i in my_string:
-        if i != "c" or i != "C":
+        if i != "c" and i != "C":
             clearedString += i
     return clearedString
