ddcf21dac94e402ceb0bc1436b2037aee32fd217
ayoub <ayoubaitelaouad@gmail.com>
2023-06-10 23:42:13 +0100

    try fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        print("{}".format(chr(ord('0') + i)))
+        print("{}".format(chr(ord('0') + i)), end="")
+        print()
+    print()
