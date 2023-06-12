8350eeb0d9bd413e2aa3c6121bed389c8f500872
ayoub <ayoubaitelaouad@gmail.com>
2023-06-12 16:00:24 +0100

    try fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,8 @@ def print_matrix_integer(matrix=[[]]):
         print()
         return
     for i in matrix:
-        print("{}".format(i[0]), end="")
+        print("{:d}".format(i[0]), end="")
         for j in i[1:]:
-            print(" {}".format(j), end="")
+            print(" {:d}".format(j), end="")
         print()
     return
