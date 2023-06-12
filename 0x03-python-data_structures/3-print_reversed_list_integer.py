e73f734bce55c5b0757d3d6412fbc019b553376e
ayoub <ayoubaitelaouad@gmail.com>
2023-06-12 15:58:29 +0100

    try fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     for i in my_list:
-        print("{:d}".format(i))
-    my_list.reverse() # return to original shap
+        print('{:d}'.format(i))
+    #my_list.reverse() # return to original shap
+    #print(my_list)
