507ffa90fd113650d4608dc521e14dfdb0f1d754
ayoub <ayoubaitelaouad@gmail.com>
2023-06-14 17:09:33 +0100

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if len(a_dictionary) == 0:
+        return None
+    max = list(a_dictionary)[0]
+    for i in a_dictionary:
+        if a_dictionary[i] > a_dictionary[max]:
+            max = i
+    return max
