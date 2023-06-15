48c1d8d7bf2018d06ada9e0006573f72e96c34cc
ayoub <ayoubaitelaouad@gmail.com>
2023-06-15 22:53:10 +0100

    hot fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 
 
+/**
+ * print_dlistint - prints a doubled list
+ * @h: head of the list
+ * Return: size of the list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
