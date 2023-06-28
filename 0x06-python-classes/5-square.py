5bdd829896bdebe7235fe73bbeb89207da69b946
ayoub <ayoubaitelaouad@gmail.com>
2023-06-28 01:39:47 +0100

    hot fix

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,7 +6,12 @@ class Square:
     """defines a square"""
     def __init__(self, size=0):
         """initializes a new square"""
-        self.size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     def area(self):
         """returns the current square area"""
