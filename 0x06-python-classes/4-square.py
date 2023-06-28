0ffed27b67de9c2d555d5ae0e7c2588c27916b83
ayoub <ayoubaitelaouad@gmail.com>
2023-06-28 01:37:28 +0100

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Classes: Square"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size=0):
+        """initializes a new square"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """retreive private attribute size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setting private attribute size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
