8f36e2b3f96a4d3bcf4f0d8d639f3ff00beba326
ayoub <ayoubaitelaouad@gmail.com>
2023-06-28 01:14:38 +0100

    hot fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""this is a module with class Square"""
+
+
 class Square:
+    """defines a square"""
     def __init__(self, size):
+        """intialize a new instance"""
         self.__size = size
