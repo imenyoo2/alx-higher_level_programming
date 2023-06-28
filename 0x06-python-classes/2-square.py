83528f231c3ee1bf44e34da9c92b280a69087657
ayoub <ayoubaitelaouad@gmail.com>
2023-06-28 01:21:11 +0100

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+"""Classes: Square"""
 
+
+class Square:
+    """defines a square"""
+    def __init__(self, size=0):
+        """initializes a new square"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
