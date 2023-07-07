f01928d48aafbe77ad2f68398006911221492ebc
ayoub <ayoubaitelaouad@gmail.com>
2023-07-07 15:19:30 +0100

    hot fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import unittest
+"""test cases for max_integer function"""
+
 
 class TestMaxInt(unittest.TestCase):
-
+    """test case for max_integer"""
     def setUp(self):
         """setup the function"""
         self.max_integer = __import__('6-max_integer').max_integer
 
     def test_normal(self):
+        """testing normal case"""
         self.assertEqual(self.max_integer([1, 2, 3, 4]), 4)
