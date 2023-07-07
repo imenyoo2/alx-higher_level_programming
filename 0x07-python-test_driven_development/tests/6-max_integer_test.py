1dbd1e817c9a73ebaf0c122aa03a9bd4a06d30be
ayoub <ayoubaitelaouad@gmail.com>
2023-07-07 16:10:37 +0100

    try fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,13 +12,15 @@ class TestMaxInteger(unittest.TestCase):
         """testing for positive testing"""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4, "max at the end")
         self.assertEqual(max_integer([4, 2, 3, 1]), 4, "max at the beginning")
+        self.assertEqual(max_integer([2, 3, 1]), 4, "max at the middle")
+        self.assertEqual(max_integer([3]), 3, "list of one element")
+        self.assertEqual(max_integer([]), None, "list is empty")
 
     def test_nigative_input(self):
         """testing for nigative number"""
         self.assertEqual(max_integer([1, -2, 3, 4]), 4, "nigative number")
         self.assertEqual(max_integer([1, -2, 3, -4]), 3, "nigative number")
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1, "nigative number")
-
 
 
 if __name__ == '__main__':
